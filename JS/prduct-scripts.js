d8e9a10af59eda8863f6f6edc80e9e1862263339
Rajabrahmam <rajchary.rc79@gmail.com>
2021-02-11 20:31:55 +0530

    Staged all changes

--- JS/prduct-scripts.js
+++ JS/prduct-scripts.js
@@ -29,10 +29,28 @@
       	}
       	slides[slideIndex-1].style.display = "block";
 	}
+	var menu=document.getElementById('menu-on-left');
+	window.onclick=(event) => {
+		if (event.target.id !== 'menu-on-left' && event.target.id !=="show-menu" ) {
+			//alert("Closing");
+			//showMenu();
+			document.getElementById('body-overlay-div').classList.remove("body-overlay");
+			document.getElementById('home-header').style.display="block";
+			document.getElementById('menu-on-left').style.marginLeft="-300px";
+			document.getElementById('mobileNav').style.position="absolute";
+			document.getElementById('mobileNav').style.transition="visibility .9s ease";
+			document.getElementById('mobileNav').style.visibility="hidden";
+			document.getElementById('mobileNav').style.opacity="0";		
+			isMenu=false;
+			show_menu.style.transform="rotate(45deg)";
+			show_menu.setAttribute('title','Explore');
+		}
+	}
 	function showMenu()
 	{
 		if(isMenu)
 		{
+			document.getElementById('body-overlay-div').classList.remove("body-overlay");
 			document.getElementById('home-header').style.display="block";
 			document.getElementById('menu-on-left').style.marginLeft="-300px";
 			document.getElementById('mobileNav').style.position="absolute";
@@ -45,6 +63,7 @@
 		}
 		else
 		{
+			document.getElementById('body-overlay-div').classList.add("body-overlay");
 			document.getElementById('home-header').style.display="none";
 			document.getElementById('menu-on-left').style.marginLeft="0px";
 			document.getElementById('mobileNav').style.transition="all .9s ease";
@@ -54,8 +73,23 @@
 			show_menu.setAttribute('title','Shrink');
 			isMenu=true;
 			show_menu.style.transform="rotate(90deg)";
+			//closeOnOutside();
 		}
+	
 		
+	}
+	
+	function closeOnOutside()
+	{
+		window.onclick=function(event)
+		{
+		if(event.target=menu)
+			{
+				//
+				alert("Closing");
+				showMenu();
+			}
+		}
 	}
 
 	function addToWishList()
